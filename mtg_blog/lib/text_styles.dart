92bc42bcef0015e319bb87837f886fa2762c5c60
Luis Rosales <luigirosales84@gmail.com>
2023-01-01 22:27:00 +0100

    added theme, text styles, and the MTG logo

---
+++ mtg_blog/lib/text_styles.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+const TextStyle title1 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 32,
+    color: Colors.black,
+    fontFamily: 'Bw Modelica');
+const TextStyle title2 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+    color: Colors.black,
+    fontFamily: 'Bw Modelica');
+const TextStyle subtitle1 = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  color: Colors.black,
+);
+const TextStyle subtitle2 = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 15,
+    color: Colors.black,
+    fontFamily: 'Bw Modelica');
+const TextStyle question1 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 24,
+    color: Colors.black,
+    fontFamily: 'Bw Modelica');
+const TextStyle answer1 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+    color: Colors.black,
+    fontFamily: 'Bw Modelica');
+const TextStyle buttonWhite = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+    color: Colors.white,
+    fontFamily: 'Bw Modelica');
+const TextStyle buttonPurple = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+    color: Colors.purple,
+    fontFamily: 'Bw Modelica');
+const TextStyle description1 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Color.fromRGBO(105, 105, 105, 1),
+    fontFamily: 'Bw Modelica',
+    overflow: TextOverflow.fade);
+const TextStyle addButton = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+    color: Color.fromRGBO(95, 166, 249, 1),
+    fontFamily: 'Bw Modelica');
+const TextStyle navbarButtons = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.white,
+    fontFamily: 'Bw Modelica');
