db5dce65be9fe198fe260abd93bfaea3d507d0a8
Luis Rosales <luigirosales84@gmail.com>
2023-01-09 20:35:13 +0100

    added error handling

--- mtg_blog/lib/api/api_cards.dart
+++ mtg_blog/lib/api/api_cards.dart
@@ -4,10 +4,13 @@ import 'package:http/http.dart' as http;
 import 'package:mtg_blog/models/card_list_model.dart';
 
 class CardAPI {
-  static Future<CardList> getCards() async {
+  static Future<Map<String, dynamic>> getCards() async {
     final res =
         await http.get(Uri.parse('https://api.magicthegathering.io/v1/cards'));
 
-    return CardList.fromJson(jsonDecode(res.body));
+    return {
+      "cards": CardList.fromJson(jsonDecode(res.body)),
+      "status_code": res.statusCode
+    };
   }
 }
